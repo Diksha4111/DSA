a39aa4b4d0fb79192e9cab71335f5957971062d3
Diksha Yadav <yadav.diksha1503@gmail.com>
2022-03-30 00:54:45 +0530

    Add files via upload

--- getMinMax.java
+++ getMinMax.java
@@ -1,12 +1,18 @@
-package max_min_array;
+package kth_min_max_array;
+import java.util.*;
 
 public class getMinMax {
 	public static void main(String[] args) {
-		int[] a = {3,5,0,1,7};
+		
+		Scanner input = new Scanner(System.in);
+		int k = input.nextInt();
+		
+		int[] a = {1,6,4,9,3,8};
 		int temp;
 		
-		for(int i=0;i<a.length;i++) {
-			for(int j=0; j<a.length-i-1;j++) {
+		for(int i=0 ;i<a.length;i++) {
+			for(int j=0;j<a.length-i-1;j++) {
+				
 				if(a[j]>a[j+1]) {
 					temp = a[j];
 					a[j] = a[j+1];
@@ -14,7 +20,7 @@ public class getMinMax {
 				}
 			}
 		}
-		System.out.print("min = " + a[0] + " " + "max = " + a[(a.length)-1]);
+		System.out.println("The kth min max are:\n");
+		System.out.print("min = "+ a[k-1] + " " + "max =" + a[(a.length)-k]);
 	}
 }
-
